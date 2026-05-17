fb6da7d76bfc3bfcccf68287be0160f17a761bc3
partha <parthakundu1973@yahoo.com>
2026-05-17 16:49:33 +0100

    fix AKS deployment workflow

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+resource "azure_resource_group"  "rg" {
+    name    =   "CICDAKS_RG"
+    location = "UK West"
+
+} 
+
+resource "azurerm_kubernetes_cluster" "aks" {
+    name    ="AKSCLUSTER"
+    location    = azurerm_resource_group.rg.location
+    resource_group_name  = azurerm_resource_group.rg.name
+    dns_prefix  = "aksdns"
+
+
+
+    default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "Standard_B2s"
+
+    }
+
+    identity {
+        type = "SystemAssigned"
+    }
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "cluster_name" {
+
+    value = azurerm_kubernetes_cluster.aks.name
+}
